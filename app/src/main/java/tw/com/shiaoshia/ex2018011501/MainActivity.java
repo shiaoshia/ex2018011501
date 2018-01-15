1079229817a89f4a84ac5c8b2257769179b0c3cb
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-15 10:10:01 +0800

    寫個類別繼承StringRequest解決中文亂碼問題

--- app/src/main/java/tw/com/shiaoshia/ex2018011501/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018011501/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -12,6 +13,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
                         img.setImageBitmap(response);
                     }
                 }, 0, 0, ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+    }
+
+    public void click02(View v) {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("https://www.mobile01.com/res/news.xml",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET",response);
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
